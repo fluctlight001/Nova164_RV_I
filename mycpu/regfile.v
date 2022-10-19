2822b2db30139c22ca2003cc3de9dfc5e31a6b3a
fluctlight001 <609804988@qq.com>
2022-10-20 00:28:10 +0800

    update 64bit regfile

--- mycpu/regfile.v
+++ mycpu/regfile.v
@@ -3,50 +3,50 @@ module regfile(
     input wire rst_n,
     
     input wire [4:0] rs1,
-    output wire [31:0] rdata1,
+    output wire [63:0] rdata1,
     input wire [4:0] rs2,
-    output wire [31:0] rdata2,
+    output wire [63:0] rdata2,
 
     input wire we,
     input wire [4:0] waddr,
-    input wire [31:0] wdata
+    input wire [63:0] wdata
 );
-    reg [31:0] rf [31:0];
+    reg [63:0] rf [31:0];
 
     always @ (posedge clk) begin
         if (!rst_n) begin
-            rf[ 0] <= 32'b0;
-            rf[ 1] <= 32'b0;
-            rf[ 2] <= 32'b0;
-            rf[ 3] <= 32'b0;
-            rf[ 4] <= 32'b0;
-            rf[ 5] <= 32'b0;
-            rf[ 6] <= 32'b0;
-            rf[ 7] <= 32'b0;
-            rf[ 8] <= 32'b0;
-            rf[ 9] <= 32'b0;
-            rf[10] <= 32'b0;
-            rf[11] <= 32'b0;
-            rf[12] <= 32'b0;
-            rf[13] <= 32'b0;
-            rf[14] <= 32'b0;
-            rf[15] <= 32'b0;
-            rf[16] <= 32'b0;
-            rf[17] <= 32'b0;
-            rf[18] <= 32'b0;
-            rf[19] <= 32'b0;
-            rf[20] <= 32'b0;
-            rf[21] <= 32'b0;
-            rf[22] <= 32'b0;
-            rf[23] <= 32'b0;
-            rf[24] <= 32'b0;
-            rf[25] <= 32'b0;
-            rf[26] <= 32'b0;
-            rf[27] <= 32'b0;
-            rf[28] <= 32'b0;
-            rf[29] <= 32'b0;
-            rf[30] <= 32'b0;
-            rf[31] <= 32'b0;
+            rf[ 0] <= 64'b0;
+            rf[ 1] <= 64'b0;
+            rf[ 2] <= 64'b0;
+            rf[ 3] <= 64'b0;
+            rf[ 4] <= 64'b0;
+            rf[ 5] <= 64'b0;
+            rf[ 6] <= 64'b0;
+            rf[ 7] <= 64'b0;
+            rf[ 8] <= 64'b0;
+            rf[ 9] <= 64'b0;
+            rf[10] <= 64'b0;
+            rf[11] <= 64'b0;
+            rf[12] <= 64'b0;
+            rf[13] <= 64'b0;
+            rf[14] <= 64'b0;
+            rf[15] <= 64'b0;
+            rf[16] <= 64'b0;
+            rf[17] <= 64'b0;
+            rf[18] <= 64'b0;
+            rf[19] <= 64'b0;
+            rf[20] <= 64'b0;
+            rf[21] <= 64'b0;
+            rf[22] <= 64'b0;
+            rf[23] <= 64'b0;
+            rf[24] <= 64'b0;
+            rf[25] <= 64'b0;
+            rf[26] <= 64'b0;
+            rf[27] <= 64'b0;
+            rf[28] <= 64'b0;
+            rf[29] <= 64'b0;
+            rf[30] <= 64'b0;
+            rf[31] <= 64'b0;
         end
         else if (we) begin
             rf[waddr] <= wdata;
@@ -54,7 +54,7 @@ module regfile(
     end
 
     // assign rdata1 = we & (rs1==waddr) ? wdata : |rs1 ? rf[rs1] : 32'b0;
-    assign rdata1 = ~(|rs1) ? 32'b0 : we & (rs1==waddr) ? wdata : rf[rs1];
+    assign rdata1 = ~(|rs1) ? 64'b0 : we & (rs1==waddr) ? wdata : rf[rs1];
     // assign rdata2 = we & (rs2==waddr) ? wdata : |rs2 ? rf[rs2] : 32'b0;
-    assign rdata2 = ~(|rs2) ? 32'b0 : we & (rs2==waddr) ? wdata : rf[rs2];
+    assign rdata2 = ~(|rs2) ? 64'b0 : we & (rs2==waddr) ? wdata : rf[rs2];
 endmodule
