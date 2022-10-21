7404d2e2217a90244e97459ab505850a0cbc6790
fluctlight001 <609804988@qq.com>
2022-10-21 17:04:48 +0800

    update soc top

--- func_test_64i/soc_sram_func/rtl/soc_lite_top.v
+++ func_test_64i/soc_sram_func/rtl/soc_lite_top.v
@@ -79,10 +79,10 @@ module soc_lite_top #(parameter SIMULATION=1'b0)
     input  [1 :0] btn_step
 );
 //debug signals
-wire [31:0] debug_wb_pc;
-wire [3 :0] debug_wb_rf_wen;
+wire [63:0] debug_wb_pc;
+wire [7 :0] debug_wb_rf_wen;
 wire [4 :0] debug_wb_rf_wnum;
-wire [31:0] debug_wb_rf_wdata;
+wire [63:0] debug_wb_rf_wdata;
 
 //clk and resetn
 wire cpu_clk;
@@ -110,29 +110,29 @@ endgenerate
 
 //cpu inst sram
 wire        cpu_inst_en;
-wire [3 :0] cpu_inst_wen;
-wire [31:0] cpu_inst_addr;
-wire [31:0] cpu_inst_wdata;
-wire [31:0] cpu_inst_rdata;
+wire [7 :0] cpu_inst_wen;
+wire [63:0] cpu_inst_addr;
+wire [63:0] cpu_inst_wdata;
+wire [63:0] cpu_inst_rdata;
 //cpu data sram
 wire        cpu_data_en;
-wire [3 :0] cpu_data_wen;
-wire [31:0] cpu_data_addr;
-wire [31:0] cpu_data_wdata;
-wire [31:0] cpu_data_rdata;
+wire [7 :0] cpu_data_wen;
+wire [63:0] cpu_data_addr;
+wire [63:0] cpu_data_wdata;
+wire [63:0] cpu_data_rdata;
 
 //data sram
 wire        data_sram_en;
-wire [3 :0] data_sram_wen;
-wire [31:0] data_sram_addr;
-wire [31:0] data_sram_wdata;
-wire [31:0] data_sram_rdata;
+wire [7 :0] data_sram_wen;
+wire [63:0] data_sram_addr;
+wire [63:0] data_sram_wdata;
+wire [63:0] data_sram_rdata;
 //conf
 wire        conf_en;
-wire [3 :0] conf_wen;
-wire [31:0] conf_addr;
-wire [31:0] conf_wdata;
-wire [31:0] conf_rdata;
+wire [7 :0] conf_wen;
+wire [63:0] conf_addr;
+wire [63:0] conf_wdata;
+wire [63:0] conf_rdata;
 
 //cpu
 mycpu_top cpu(
@@ -164,33 +164,33 @@ inst_ram inst_ram
 (
     .clka  (cpu_clk            ),   
     .ena   (cpu_inst_en        ),
-    .wea   (cpu_inst_wen       ),   //3:0
-    .addra (cpu_inst_addr[19:2]),   //17:0
-    .dina  (cpu_inst_wdata     ),   //31:0
-    .douta (cpu_inst_rdata     )    //31:0
+    .wea   (cpu_inst_wen       ),   //7:0
+    .addra (cpu_inst_addr[16:3]),   //17:0
+    .dina  (cpu_inst_wdata     ),   //63:0
+    .douta (cpu_inst_rdata     )    //63:0
 );
 
 bridge_1x2 bridge_1x2(
     .clk             ( cpu_clk         ), // i, 1                 
     .resetn          ( cpu_resetn      ), // i, 1                 
 
-    .cpu_data_en     ( cpu_data_en     ), // i, 4                 
-    .cpu_data_wen    ( cpu_data_wen    ), // i, 4                 
-    .cpu_data_addr   ( cpu_data_addr   ), // i, 32                
-    .cpu_data_wdata  ( cpu_data_wdata  ), // i, 32                
-    .cpu_data_rdata  ( cpu_data_rdata  ), // o, 32                
+    .cpu_data_en     ( cpu_data_en     ), // i, 1                 
+    .cpu_data_wen    ( cpu_data_wen    ), // i, 8                 
+    .cpu_data_addr   ( cpu_data_addr   ), // i, 64                
+    .cpu_data_wdata  ( cpu_data_wdata  ), // i, 64                
+    .cpu_data_rdata  ( cpu_data_rdata  ), // o, 64                
 
-    .data_sram_en    ( data_sram_en    ), // o, 4                 
-    .data_sram_wen   ( data_sram_wen   ), // o, 4                 
-    .data_sram_addr  ( data_sram_addr  ), // o, `DATA_RAM_ADDR_LEN
-    .data_sram_wdata ( data_sram_wdata ), // o, 32                
-    .data_sram_rdata ( data_sram_rdata ), // i, 32                
+    .data_sram_en    ( data_sram_en    ), // o, 1                 
+    .data_sram_wen   ( data_sram_wen   ), // o, 8                 
+    .data_sram_addr  ( data_sram_addr  ), // o, 64
+    .data_sram_wdata ( data_sram_wdata ), // o, 64                
+    .data_sram_rdata ( data_sram_rdata ), // i, 64                
 
     .conf_en         ( conf_en         ), // o, 1                 
     .conf_wen        ( conf_wen        ), // o, 4                 
-    .conf_addr       ( conf_addr       ), // o, 32                
-    .conf_wdata      ( conf_wdata      ), // o, 32                
-    .conf_rdata      ( conf_rdata      )  // i, 32                
+    .conf_addr       ( conf_addr       ), // o, 64                
+    .conf_wdata      ( conf_wdata      ), // o, 64                
+    .conf_rdata      ( conf_rdata      )  // i, 64                
  );
 
 //data ram
@@ -198,10 +198,10 @@ data_ram data_ram
 (
     .clka  (cpu_clk             ),   
     .ena   (data_sram_en        ),
-    .wea   (data_sram_wen       ),   //3:0
-    .addra (data_sram_addr[17:2]),   //15:0
-    .dina  (data_sram_wdata     ),   //31:0
-    .douta (data_sram_rdata     )    //31:0
+    .wea   (data_sram_wen       ),   //7:0
+    .addra (data_sram_addr[16:3]),   //15:0
+    .dina  (data_sram_wdata     ),   //63:0
+    .douta (data_sram_rdata     )    //63:0
 );
 
 //confreg
@@ -212,9 +212,9 @@ confreg #(.SIMULATION(SIMULATION)) confreg
     .resetn      ( cpu_resetn ),  // i, 1    
     .conf_en     ( conf_en    ),  // i, 1      
     .conf_wen    ( conf_wen   ),  // i, 4      
-    .conf_addr   ( conf_addr  ),  // i, 32        
-    .conf_wdata  ( conf_wdata ),  // i, 32         
-    .conf_rdata  ( conf_rdata ),  // o, 32         
+    .conf_addr   ( conf_addr  ),  // i, 64        
+    .conf_wdata  ( conf_wdata ),  // i, 64         
+    .conf_rdata  ( conf_rdata ),  // o, 64         
     .led         ( led        ),  // o, 16   
     .led_rg0     ( led_rg0    ),  // o, 2      
     .led_rg1     ( led_rg1    ),  // o, 2      
