176f3bf8f365b5744b586e49a196ab4f0443f92a
yunlang <yunlanglang@outlook.com>
2022-11-30 21:17:28 +0800

    add comment about lsu

--- mycpu/pipeline/lsu.v
+++ mycpu/pipeline/lsu.v
@@ -14,24 +14,27 @@ module lsu(
 
     wire data_ram_en;
     wire data_ram_we;
+    // {double word, word, half word, byte}
     wire [3:0] data_size_sel;
     wire data_unsigned;
 
     wire [7:0] byte_sel;
+
 assign {
         data_ram_en, data_ram_we, data_size_sel, data_unsigned
     } = lsu_op;
     
-    // decoder_2_4 u_decoder_2_4(
-    // 	.in  (data_sram_addr[1:0]),
-    //     .out (byte_sel           )
-    // );
+    // implement addr convert to byte sel
     decoder_3_8 u_decoder_3_8(
     	.in  (data_sram_addr[2:0]  ),
         .out (byte_sel             )
     );
     
-
+    // when byte, select with byte_sel(addr -> byte_sel one hot)
+    // when half word, the addr may be 000(0), 010(2), 100(4), 110(6). two hot
+    // when word, the addr may be 000(0), 100(4), four hot
+    // when double word, the addr may be 000(0), eight hot
+    // waring：mismatch between addr and data_size could be exception。
     assign data_ram_sel = data_size_sel[0] ? byte_sel 
                         : data_size_sel[1] ? {{2{byte_sel[6]}},{2{byte_sel[4]}},{2{byte_sel[2]}},{2{byte_sel[0]}}} 
                         : data_size_sel[2] ? {{4{byte_sel[4]}},{4{byte_sel[0]}}}
@@ -41,6 +44,7 @@ assign {
     assign data_sram_en     = data_ram_en;
     assign data_sram_we    = {8{data_ram_we}} & data_ram_sel;
     assign data_sram_addr   = rdata1 + imm;
+    // a trick, copy the data and select with enable
     assign data_sram_wdata  = data_size_sel[0] ? {8{rdata2[7:0]}} 
                             : data_size_sel[1] ? {4{rdata2[15:0]}} 
                             : data_size_sel[2] ? {2{rdata2[31:0]}} 
